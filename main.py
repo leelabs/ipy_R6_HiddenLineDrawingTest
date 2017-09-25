688724d987769688bdd706ba790f9d346b9187aa
bjlee2 <brian.j.lee2@gmail.com>
2017-09-24 20:17:05 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import Rhino
+import clr
+from System.Collections.Generic import List
+#clr.AddReference(
+#clr.AddReferenceByName("geom")
+#clr.Reference("geom")
+#geom = Rhino.Geometry.GeometryBase()
+#List<Rhino.Geometry.GeometryBase> geom = new List<Rhino.Geometry.GeometryBase>()
+
+filter = Rhino.DocObjects.ObjectType.Brep
+
+objref = clr.Reference[Rhino.DocObjects.ObjRef]()
+object = Rhino.Input.RhinoGet.GetOneObject("Get It!", False, filter, objref)
+
+viewport = Rhino.Display.RhinoViewport()
+
+options = Rhino.Geometry.HiddenLineDrawingParameters()
+options.SetViewport(viewport)
+options.AddGeometry(objref.Geometry(),"")
+
+drawing = Rhino.Geometry.HiddenLineDrawing.Compute(options, True)
+
+print drawing
